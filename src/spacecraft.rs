7bcfefcc90b320ba51a36329d65a3d853c3e84fc
Aamer Mohammed <aamerm@thoughtworks.com>
2019-12-02 21:04:16 +0530

    read from file day1

--- src/spacecraft.rs
+++ src/spacecraft.rs
@@ -14,10 +14,12 @@ use std::fs;
 //    Ok(())
 //}
 
-fn read_from_file(filename: String) -> io::Result<Vec<String>> {
+fn read_from_file(filename: String) -> io::Result<Vec<i64>> {
     let file_in = fs::File::open(filename)?;
     let file_reader = BufReader::new(file_in);
-    Ok(file_reader.lines().filter_map(io::Result::ok).collect())
+    let input_vector: Vec<String> = file_reader.lines().filter_map(io::Result::ok).collect();
+    let result = input_vector.iter().map(|x| x.parse::<i64>().unwrap()).collect::<Vec<_>>();
+    return Ok(result);
 }
 
 pub fn spacecraft() {
@@ -25,28 +27,23 @@ pub fn spacecraft() {
     //fuel for each module - for each item in list, divide by 3 round down, subtract 2 - inputlist.map and return newlist
     //totalfuel - newlist.reduce
 
-//    let file_path = String::from("input_day1.data");
-//    let module_mass: Vec<i64> = read_from_file(file_path);
-//    match read_from_file(file_path) {
-//        Ok(data) => convert_to_int(data),
+    let file_path = String::from("src/resources/input_day1.txt");
+    let module_mass = match read_from_file(file_path) {
+        Ok(data) => data,
+        Err(err) => Vec::new()
 //        Err(err) => println!("err {}", err)
-//    };
+    };
 
-    let module_mass: Vec<i64> = vec![12, 14, 1969, 100756];
+//    let module_mass: Vec<i64> = vec![12, 14, 1969, 100756];
     fuel_counter_upper(module_mass);
 }
 
-fn convert_to_int(data: Vec<String>) {
-    let result = data.iter().map(|x| x.parse::<i64>().unwrap()).collect::<Vec<_>>();
-}
-
 fn fuel_counter_upper(module_mass: Vec<i64>) {
-
 //    let v = u.iter().map(|&x| x + 1).collect::<Vec<_>>();
     let module_fuel = module_mass.into_iter().map(calculate_fuel).collect::<Vec<_>>();
     let total_fuel: i64 = module_fuel.iter().sum();
 
-    print_vector(module_fuel);
+//    print_vector(module_fuel);
     println!("{}", total_fuel);
 }
 
@@ -54,8 +51,8 @@ fn calculate_fuel(mass: i64) -> i64 {
     (mass/3)-2
 }
 
-fn print_vector(v: Vec<i64>) {
-    for x in v.iter() {
-        println!("vec contained {}", x);
-    }
-}
+//fn print_vector(v: Vec<i64>) {
+//    for x in v.iter() {
+//        println!("vec contained {}", x);
+//    }
+//}
