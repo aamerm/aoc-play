59e58839a36abcc9d633cf2eba5feeb7e1cd6ad5
Aamer Mohammed <aamerm@thoughtworks.com>
2019-12-04 16:42:40 +0530

    day4-part2

--- src/securecontainer.rs
+++ src/securecontainer.rs
@@ -10,7 +10,7 @@ pub fn password_finder() {
     println!("{}", end);
 
     for value in start..end {
-        if never_decrease(value) && has_double(value) {
+        if never_decrease(value) && has_double(value) && has_strict_double(value) {
             println!("Possible password is: {} ", value);
             result += 1;
         }
@@ -18,6 +18,10 @@ pub fn password_finder() {
 
 //    println!("{}", never_decrease(123789));
 //    println!("{}", has_double(123789));
+//    println!("{}", has_strict_double(112233));
+//    println!("{}", has_strict_double(123444));
+//    println!("{}", has_strict_double(111122));
+//    println!("{}", has_strict_double(588889));
 
     println!("Count of possible passwords: {}", result);
 }
@@ -40,7 +44,36 @@ fn has_double(value: u32) -> bool {
         }
     }
     false
+}
 
+fn has_strict_double(value: u32) -> bool {
+    let vec_number: Vec<u32> = string_number_to_vec(value);
+
+    let mut i = 0;
+    let mut done = false;
+    while !done {
+        let mut curr_count = 1;
+        for j in i+1..vec_number.len() {
+            if vec_number[i] == vec_number[j] {
+                curr_count += 1;
+            } else if vec_number[i] != vec_number[j] {
+                i = j;
+                break;
+            }
+
+            if j == vec_number.len()-1 {
+                i = j;
+                break;
+            }
+        }
+        if curr_count == 2 {
+            return true;
+        }
+        if i >= vec_number.len()-1 {
+            done = true;
+        }
+    }
+    return false;
 }
 
 //fn number_to_vec(n: u32) -> Vec<u32> {
